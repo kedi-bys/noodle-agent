16a6c9edf709ddb4b995e80782b3348689f7f151
Sefa Koç <koc.sefa@hotmail.com>
2019-12-12 09:26:13 +0300

    Event eklemeleri yapıldı.

--- src/IpcListener.js
+++ src/IpcListener.js
@@ -1,13 +1,25 @@
 module.exports = (ipcRenderer, socketManager, logger) => {
-  ipcRenderer.on('device-unlocked', () => {
-    logger.Warning(`Oturum açıldı.`)
-    socketManager.TryConnect()
-  })
-
-  // Cihaz uyandığında socket bağlantısı muhtemelen kopmuş olacak. Bu event
-  // geldiğinde SocketManager'ın tekrar bağlanmasını sağlamak lazım. Eğer
-  // kendisi otomatik olarak yapmıyorsa.
-  ipcRenderer.on('device-resumed', () => {
-    socketManager.TryConnect()
-  })
+  ipcRenderer
+    .on('device-suspend', () => {
+      logger.Warning(`Cihaz uyku moduna geçti.`)
+    })
+    // Cihaz uyandığında socket bağlantısı muhtemelen kopmuş olacak. Bu event
+    // geldiğinde SocketManager'ın tekrar bağlanmasını sağlamak lazım. Eğer
+    // kendisi otomatik olarak yapmıyorsa.
+    .on('device-resumed', () => {
+      logger.Warning(`Cihaz uyandırıldı.`)
+      socketManager.TryConnect()
+    })
+    .on('device-on-ac', () => {
+      logger.Warning(`Cihaz AC gücüyle çalışıyor.`)
+    })
+    .on('device-on-battery', () => {
+      logger.Warning(`Cihaz batarya gücüyle çalışıyor.`)
+    })
+    .on('device-locked', () => {
+      logger.Warning(`Oturum kapandı.`)
+    })
+    .on('device-unlocked', () => {
+      logger.Warning(`Oturum açıldı.`)
+    })
 }
