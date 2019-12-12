df225bb01f93194ddc8c7eb64bbafe7e26bc9653
Sefa Koç <koc.sefa@hotmail.com>
2019-12-12 18:09:55 +0300

    Event'lar socket ile sunucuya gönderiliyor

--- src/IpcListener.js
+++ src/IpcListener.js
@@ -1,31 +1,36 @@
 module.exports = (ipcRenderer, socketManager, logger) => {
   ipcRenderer
     .on('device-suspend', () => {
-      logger.Warning(`Cihaz uyku moduna geçti.`)
+      logger.Warning('Cihaz uyku moduna geçti.')
     })
     // Cihaz uyandığında socket bağlantısı muhtemelen kopmuş olacak. Bu event
     // geldiğinde SocketManager'ın tekrar bağlanmasını sağlamak lazım. Eğer
     // kendisi otomatik olarak yapmıyorsa.
     .on('device-resumed', () => {
-      logger.Warning(`Cihaz uyandırıldı.`)
+      logger.Warning('Cihaz uyandırıldı.')
       socketManager.TryConnect()
     })
     .on('device-on-ac', () => {
-      logger.Warning(`Cihaz AC gücüyle çalışıyor.`)
+      logger.Warning('Cihaz AC gücüyle çalışıyor.')
+      socketManager.Emit('device-on-ac', 'Cihaz AC gücüyle çalışıyor.')
     })
     .on('device-on-battery', () => {
-      logger.Warning(`Cihaz batarya gücüyle çalışıyor.`)
+      logger.Warning('Cihaz batarya gücüyle çalışıyor.')
     })
     .on('device-locked', () => {
-      logger.Warning(`Oturum kapandı.`)
+      logger.Warning('Oturum kilitli.')
+      socketManager.Emit('device-locked','Oturum Kilitli.')
     })
     .on('device-unlocked', () => {
-      logger.Warning(`Oturum açıldı.`)
+      logger.Warning('Oturum açıldı.')
+      socketManager.Emit('device-unlocked', 'Oturum Açıldı.')
     })
     .on('user-active', () => {
-      logger.Success(`Kullanıcı aktif durumda.`)
+      logger.Success('Kullanıcı aktif durumda.')
+      socketManager.Emit('user-active', 'Kullanıcı aktif durumda.')
     })
     .on('user-idle', () => {
-      logger.Warning(`Kullanıcı bekleme durumunda.`)
+      logger.Warning('Kullanıcı bekleme durumunda.') 
+      socketManager.Emit('user-idle', 'Kullanıcı bekleme durumda.')     
     })
 }
