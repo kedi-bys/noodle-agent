98303fb4332badc59955aac4b92ed32fa79bca76
Sefa Koç <koc.sefa@hotmail.com>
2019-12-16 09:52:27 +0300

    enviroment fs ile yapıldı.

--- application.js
+++ application.js
@@ -1,18 +1,21 @@
 const { ipcRenderer } = require('electron')
-const dotenv = require('dotenv')
+// const dotenv = require('dotenv')
+const fs = require('fs')
 const LogManager = require('./src/LogManager')
 const SocketManager = require('./src/SocketManager')
 const DataManager = require('./src/DataManager')
 
 // env variable'ları oluştur
-dotenv.config()
+// dotenv.config()
+let rawenv = fs.readFileSync('./resources/env.json')
+let env = JSON.parse(rawenv)
 
 const logger = new LogManager()
 
 // Agent başlatıldı bilgisi
 logger.Success(`Agent çalışıyor.`)
 
-const socketManager = new SocketManager(process.env.SERVER, logger)
+const socketManager = new SocketManager(env.SERVER, logger)
 const dataManager = new DataManager(logger)
 const ipcListener = require('./src/IpcListener')(ipcRenderer, socketManager, logger)
 
